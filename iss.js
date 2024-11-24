b1055601a4a7967f7e7261eab7aa4d35d2b0d219
Andrew Roth <a.roth.80@hotmail.com>
2024-11-24 13:04:19 -0800

    working fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -7,7 +7,7 @@
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 const needle = require('needle');
-
+/*
 const fetchMyIP = function(callback) {
   needle.get('https://api.ipify.org?format=json', (error, response, body) => {
     if (error) return callback(error, null);
@@ -21,5 +21,26 @@ const fetchMyIP = function(callback) {
     callback(null, ip);
   });
 };
+*/
+const fetchCoordsByIP = function(ip, callback) {
+  needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
 
-module.exports = { fetchMyIP };
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const latitude = body.latitude
+    const longitude = body.longitude
+    callback(null, {latitude, longitude});
+  });
+};
+
+module.exports = {fetchCoordsByIP};
+//module.exports = { fetchMyIP };
