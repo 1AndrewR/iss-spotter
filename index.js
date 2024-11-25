fb03091da05bc2b0205fd2e8d59917e0d9818563
Andrew Roth <a.roth.80@hotmail.com>
2024-11-24 18:44:36 -0800

    code cleaned up

--- index.js
+++ index.js
@@ -34,13 +34,3 @@ fetchMyIP((error, ip) => {
     });
   });
 });
-
-
-
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  // success, print out the deets!
-  console.log(passTimes);
-});
